53a6545cbe592ea328b48fde4f8ee114a44269e2
VikasRaskar <vikasraskar9273@gmail.com>
2022-04-29 12:58:27 +0530

    update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,15 +1,12 @@
 let express = require("express");
 let app = require("express")();
-
-let mongoose = require("mongoose");
 let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
 let cors = require("cors");
 const SignInModel = require('./model/admin/signin.model');
-
 app.use(express.static(process.cwd()));
 
-const PORT = 3001 
-
+const PORT = 3001;
 let url = "mongodb://localhost:27017/groceryStore";
 
 
@@ -18,14 +15,14 @@ app.use(bodyParser.json());
 app.use(cors());          
 
 
-const mongooseDb ={       
+const mongooseDbOption ={       
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
-mongoose.connect(url,mongooseDb);   
+mongoose.connect(url,mongooseDbOption);   
 mongoose.connection
 
-
+//Generate admin credentials if not present
 SignInModel.find({},async (error,data)=>{
     if(data.length==0){
         adminLogin = new SignInModel();
@@ -35,17 +32,29 @@ SignInModel.find({},async (error,data)=>{
     }
 });
 
-
-
+//connecting with the routers
+var Product = require("./router/admin/product.router.js");
 var adminSignIn=require("./router/admin/signin.router.js");
 var User = require("./router/user/user.router.js");
 const Employee = require("./router/employee/employee.router.js");
+const Reports = require("./router/admin/reports.router.js");
+const ProdRequests = require("./router/admin/requests.router.js");
+var TicketRequest = require("./router/user/user.router")
+var Orders = require("./router/user/user.router")
 const empSignIn = require("./router/employee/signin.router.js");
 
 
+
+app.use("/admin/product",Product);
 app.use("/admin",adminSignIn);
 app.use("/admin/employee",Employee);
-app.use("/user", User);
-app.use("/employee", Employee)
+app.use("/admin/reports",Reports);
+app.use("/admin",ProdRequests);
 
-app.listen(PORT,()=>console.log(`Server running on port number ${PORT}`));
+app.use("/user", User);
+app.use("/user/orderSelected", Orders);
+app.use("/user/addTicket" , TicketRequest );
+
+app.use("/employee", Employee);
+
+app.listen(PORT,()=>console.log(`Server is running on port No. ${PORT}`));
